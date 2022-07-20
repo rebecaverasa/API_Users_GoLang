bfa51d50882aeeb849629af5687a992bec327003
Rebeca Veras <rebecaverasa@gmail.com>
2022-07-20 16:21:53 -0300

    att

--- main.go
+++ main.go
@@ -51,55 +51,54 @@ func getUsers(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, users)
 }
 
-
-func createUser(c *gin.Context) {
+func createUser(context *gin.Context) {
 	var newUser user
 
-	if err := c.BindJSON(&newUser); err != nil { 
-		return 
-	} 
+	err := context.BindJSON(&newUser); 
+	if err != nil {
+		return
+	}
 
-	users = append(users, newUser)              
-	c.IndentedJSON(http.StatusCreated, newUser)
-
+	users = append(users, newUser)
+	context.IndentedJSON(http.StatusCreated, newUser)
 }
 
 func getUser(context *gin.Context) {
-	id := context.Param("id")
-	user, err := getUserById((id))
+	id := context.Param("id")      //pega o id que digitei na url
+	user, err := getUserById((id)) //é o parametro id criado acima. E Acontece uma atribuição dupla: User e err pegam respectivamente 2 parametros da funçao getusersbyid: user e error
 
 	if err != nil {
 		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
 		return
 	}
 
-	context.IndentedJSON(http.StatusOK, user)
+	context.IndentedJSON(http.StatusOK, user) //retorna o user do getuserbyid
 }
 
-func getUserById(id string) (*user, error) {
-	for i, u := range users {
-		if u.Id == id {
-			return &users[i], nil
+func getUserById(id string) (*user, error) { //2 retornos: tipo user e tipo error.
+	for i, u := range users { //range percorre todo o users. u é 1 elemento de users que modifica a cada interação do for.
+		if u.Id == id { //u.Id pega só o Id do users e compara com o parametro de entrada. Ex:/2
+			return &users[i], nil //o u vai procurar em looping o Id de entrada até encontrar.Quando achar, vai retnornar o usuario nesta posição e erro nil.
 		}
 	}
 
-	return nil, errors.New("User not found")
+	return nil, errors.New("User not found") //quando percorrer tudo e nao encontrar o parametro de entrada, retornara um usuario nil e a mensagem de erro.
 }
 
-func deleteUserById(c *gin.Context) {
-	id := c.Param("id")
+func deleteUserById(context *gin.Context) {
+	id := context.Param("id") //parametro id da request delete.
 
 	for i, a := range users {
 		if a.Id == id {
-			aux := users[i+1:]
-			users = append(users[:i], aux...)
+			aux := users[i+1:]                //aqui pega os usuarios depois do usuario que digitei
+			users = append(users[:i], aux...) //o users deixa de ser o elemento completo e passa a ser o users sem o numero que quero deletar
 
-			c.IndentedJSON(http.StatusOK, users)
+			context.IndentedJSON(http.StatusOK, users)
 			return
 		}
 	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
-
 
 func main() {
 	router := gin.Default()
